280e4d3144b2cc04d76d5c0b5ec7644f9a6aa757
= viper <ygaurabh@gmail.com>
2025-10-29 11:42:51 +0545

    add a hovering effect

--- sketch.js
+++ sketch.js
@@ -6,3 +6,12 @@ for (let j = 0; j < 16; j++) {
     container.append(element);
   }
 }
+
+function random(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+container.addEventListener("mouseover", (e) => {
+  const rndClr = `rgb(${random(255)} , ${random(255)} , ${random(255)})`;
+  e.target.style.backgroundColor = rndClr;
+});
